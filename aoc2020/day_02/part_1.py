7be11a979d7983870c4b70709a83c85feb207819
Ian Laird <irlaird@gmail.com>
2020-12-01 22:47:23 -0700

    solution for day 2 part 1

--- aoc2020/day_02/part_1.py
+++ aoc2020/day_02/part_1.py
@@ -1,9 +1,17 @@
 from aoc2020 import *
 
 
-class Solution(SolutionABC):
+def check_pw(line: str):
+    policy, pw = line.split(': ', 2)
+    limits, letter = policy.split(' ')
+    lower, upper = limits.split('-')
+    return 1 if int(lower) <= pw.count(letter) <= int(upper) else 0
 
-    expected = None
+
+class Solution(SolutionABC):
+    """ How many passwords are valid according to their policies? """
+
+    expected = 2
 
     def solve(self) -> any:
-        raise NoSolutionError()
+        return sum(self.resource_lines("input", check_pw))
