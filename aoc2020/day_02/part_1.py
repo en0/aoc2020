6e1e8c8e86d9ade396f5142321db7d3ab37ece9a
Ian Laird <irlaird@gmail.com>
2020-12-01 23:08:46 -0700

    solution for day 2 part 2

--- aoc2020/day_02/part_1.py
+++ aoc2020/day_02/part_1.py
@@ -1,17 +1,17 @@
 from aoc2020 import *
 
 
-def check_pw(line: str):
-    policy, pw = line.split(': ', 2)
-    limits, letter = policy.split(' ')
-    lower, upper = limits.split('-')
-    return 1 if int(lower) <= pw.count(letter) <= int(upper) else 0
-
-
 class Solution(SolutionABC):
     """ How many passwords are valid according to their policies? """
 
     expected = 2
 
     def solve(self) -> any:
-        return sum(self.resource_lines("input", check_pw))
+        return sum(self.resource_lines("input", self.check_pw))
+
+    @classmethod
+    def check_pw(cls, line: str):
+        policy, pw = line.split(': ', 2)
+        limits, letter = policy.split(' ')
+        lower, upper = limits.split('-')
+        return 1 if int(lower) <= pw.count(letter) <= int(upper) else 0
--- aoc2020/day_02/part_2.py
+++ aoc2020/day_02/part_2.py
@@ -1,9 +1,17 @@
-from aoc2020 import *
+from .part_1 import Solution as Part1Solution
 
 
-class Solution(SolutionABC):
+class Solution(Part1Solution):
 
-    expected = None
+    expected = 1
 
-    def solve(self) -> any:
-        raise NoSolutionError()
+    @classmethod
+    def check_pw(cls, line: str):
+        policy, pw = line.split(': ', 2)
+        pos, letter = policy.split(' ')
+        pos1, pos2 = pos.split('-')
+        return sum([
+            pw[int(pos1)-1] == letter,
+            pw[int(pos2)-1] == letter,
+        ]) == 1
+
