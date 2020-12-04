e205e7c1d3531f1aed81e9bcfda844cb145a8e4b
Ian Laird <irlaird@gmail.com>
2020-12-04 00:21:22 -0700

    Solution for day 4 part 1,2

--- aoc2020/day_04/part_1.py
+++ aoc2020/day_04/part_1.py
@@ -2,8 +2,36 @@ from aoc2020 import *
 
 
 class Solution(SolutionABC):
-
-    expected = None
+    expected = 2
 
     def solve(self) -> any:
-        raise NoSolutionError()
+        ret, buffer = 0, []
+        for line in self.resource_lines("input"):
+            if line != "":
+                [buffer.append(_) for _ in line.split(" ")]
+                continue
+            elif self.is_valid(buffer):
+                ret += 1
+            buffer = []
+
+        if buffer and self.is_valid(buffer):
+            ret += 1
+        return ret
+
+    @classmethod
+    def is_valid(cls, buffer):
+        required_fields = {
+            'byr': False,
+            'iyr': False,
+            'eyr': False,
+            'hgt': False,
+            'hcl': False,
+            'ecl': False,
+            'pid': False,
+            'cid': True,
+        }
+
+        for field, _ in map(lambda f: f.split(":"), buffer):
+            required_fields[field] = True
+
+        return all(required_fields.values())
--- aoc2020/day_04/part_2.py
+++ aoc2020/day_04/part_2.py
@@ -1,9 +1,56 @@
-from aoc2020 import *
+import re
+from .part_1 import Solution as Part1Solution
 
 
-class Solution(SolutionABC):
+def between(low, hi, val):
+    return low <= int(val) <= hi
 
-    expected = None
 
-    def solve(self) -> any:
-        raise NoSolutionError()
+def re_validate(pattern, value):
+    return re.fullmatch(pattern, value) is not None
+
+
+def re_validate_groups(pattern, value, check):
+    match = re.fullmatch(pattern, value)
+    return False if match is None else check(*match.groups())
+
+
+hgt_validators = {
+    'cm': lambda b: between(150, 193, b),
+    'in': lambda b: between(59, 76, b),
+}
+
+field_validator = {
+    'byr': lambda b: between(1920, 2002, b),
+    'iyr': lambda b: between(2010, 2020, b),
+    'eyr': lambda b: between(2020, 2030, b),
+    'hgt': lambda b: re_validate_groups(
+        r"(\d*)(cm|in)", b, lambda v, u: hgt_validators.get(u, lambda x: False)(v)),
+    'hcl': lambda b: re_validate(r"#[a-f0-9]{6}", b),
+    'ecl': lambda b: b in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
+    'pid': lambda b: re_validate(r"\d{9}", b),
+    'cid': lambda b: True,
+}
+
+
+class Solution(Part1Solution):
+    expected = 2
+
+    @classmethod
+    def is_valid(cls, buffer):
+
+        required_fields = {
+            'byr': False,
+            'iyr': False,
+            'eyr': False,
+            'hgt': False,
+            'hcl': False,
+            'ecl': False,
+            'pid': False,
+            'cid': True,
+        }
+
+        for field, val in map(lambda f: f.split(":"), buffer):
+            required_fields[field] = field_validator.get(field, lambda x: True)(val)
+
+        return all(required_fields.values())
