40974893fe54bee01aa295e0c2a69e7d11e37723
Ian Laird <irlaird@gmail.com>
2020-12-15 18:28:48 -0700

    cleaned up solution for day 15 to reduce calls. Trying to eek out a bit more performance.

--- aoc2020/day_15/part_1.py
+++ aoc2020/day_15/part_1.py
@@ -1,31 +1,21 @@
 from aoc2020 import *
-from collections import deque
-from typing import Dict, Deque
 
 
 class Solution(SolutionABC):
     expected = 436
-    spoken_map: Dict[int, Deque[int]] = {}
-    last_spoken: int = None
-    spoken_len: int = 0
 
     def solve(self) -> any:
         return self.spoken_at(2020)
 
     def spoken_at(self, t):
-        self.spoken_len = len([self.speak(_) for _ in self.resource_lines("input", int)])
-        while self.spoken_len < t:
-            self.turn()
-        return self.last_spoken
-
-    def turn(self):
-        indices = self.spoken_map[self.last_spoken]
-        if len(indices) > 1:
-            self.speak(indices[-1] - indices[-2])
-        else:
-            self.speak(0)
-
-    def speak(self, val):
-        self.spoken_len += 1
-        self.last_spoken = val
-        self.spoken_map.setdefault(val, deque(maxlen=2)).append(self.spoken_len)
+        i, i_map = 0, {}
+        for val in self.resource_lines("input", int):
+            i += 1
+            i_map[val] = i
+        said = val
+        del i_map[val]
+        while i < t:
+            say = i - i_map[said] if said in i_map else 0
+            i_map[said] = i
+            said, i = say, i + 1
+        return said
--- aoc2020/day_15/part_2.py
+++ aoc2020/day_15/part_2.py
@@ -5,8 +5,5 @@ class Solution(Part1):
     expected = 436
 
     def solve(self) -> any:
-        """
-        I am sure there is a better way to do this. Must revisit this weekend.
-        The below takes about 60 seconds on a 5 year old hardware /shrug
-        """
+        # /shrug
         return self.spoken_at(30000000)
